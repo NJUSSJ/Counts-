4a6a3cd23b8e2f15f085df5921825afd7bf1ee24
wangruihua <161250143@smail.nju.edu.cn>
2018-04-09 19:59:03 +0800

    建立了通用的增删改查Service
    Signed-off-by: wrh <161250143@smail.nju.edu.cn>

---
+++ src/main/java/com/seproject/service/BasicBLService.java
@@ -0,0 +1,18 @@
+package com.seproject.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BasicBLService<T> {
+    /*
+    * 基本业务逻辑---增删改查的实现
+    * 使用泛型加载领域对象
+    * 用于实现数据库中对象的增加、修改、删除、精确查找、模糊查找
+     */
+ /*   UserService service;
+    @Autowired
+    public void setService(UserService service){this.service=service;}
+    这段代码不知道对不对
+    */
+}
--- src/main/java/com/seproject/service/Key.java
+++ src/main/java/com/seproject/service/Key.java
@@ -1,5 +1,12 @@
 package com.seproject.service;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface Key {
     //主键注解，加在领域对象的成员变量上。
     //要求所加的变量是基本数据类型或String类型。
