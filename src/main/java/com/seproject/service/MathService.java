a121caca70e555b310b56511edad6ae8495bd36a
wangruihua <161250143@smail.nju.edu.cn>
2018-04-14 14:40:32 +0800

    。
    Signed-off-by: wrh <161250143@smail.nju.edu.cn>

---
+++ src/main/java/com/seproject/service/MathService.java
@@ -0,0 +1,32 @@
+package com.seproject.service;
+
+public class MathService<T> {
+    /**
+     * 并运算
+     */
+
+
+    /**
+     * 交运算
+     */
+
+    /**
+     * 差运算
+     */
+
+    /**
+     *升序排序
+     * */
+
+    /**
+     * 降序排序
+     */
+
+    /**
+     * json字符串 转对象
+     */
+
+    /**
+     * 对象 转json字符串
+     */
+}
