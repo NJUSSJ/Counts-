1a53089bab1c8cb41695c693041a1a9f2e03bc67
wangruihua <161250143@smail.nju.edu.cn>
2018-04-23 10:08:27 +0800

    V2.5.2  帮写好了一个装填标注信息的方法 在Statistic Service
    Signed-off-by: wangruihua <161250143@smail.nju.edu.cn>

--- src/main/java/com/seproject/service/StatisticsService.java
+++ src/main/java/com/seproject/service/StatisticsService.java
@@ -1,5 +1,7 @@
 package com.seproject.service;
 import com.seproject.domain.Collection;
+import com.seproject.domain.ImageInfo;
+import com.seproject.domain.Mission;
 import com.seproject.domain.StatisticsData.*;
 import com.seproject.domain.User;
 import org.springframework.stereotype.Service;
@@ -18,6 +20,7 @@ public class StatisticsService {
      */
     BasicBLService<Collection> service1=new BasicBLService<Collection>(new Collection());
     BasicBLService<User> service2= new BasicBLService<User>(new User());
+    BasicBLService<Mission> service3=new BasicBLService<Mission>(new Mission());
     public WorkerData getWorkerData(String userID){
         WorkerData w=new WorkerData();
 
@@ -35,5 +38,24 @@ public class StatisticsService {
     }
     public SingleMissionData getSingleMissionData(String missionID){
         return null;
+
+    }
+
+    /**
+     * 根据 Image id 自动将其封装在对应的Collection 里
+     */
+    public RM fillCollection (ImageInfo imageInfo)
+    {
+        String temp0[]=imageInfo.getImgid().split("-");
+        String missionName=temp0[0];
+        String picName0= temp0[1]; //这个属性必须是数字,且最好从0开始
+        String userName= temp0[2];
+
+        Collection collection=service1.findByKey(missionName+userName);
+        ArrayList<ImageInfo> infoList=collection.getInfoList();
+        infoList.set(Integer.parseInt(picName0),imageInfo);
+        collection.setInfoList(infoList);
+        service1.update(collection);
+        return RM.SUCCESS;
     }
 }
