1b0f91912b6b2cfdd3a9132d8f0e77901f2e03ba
SSJ <njussj@gmail.com>
2018-04-24 19:56:12 +0800

    fix conflicts

--- src/main/java/com/seproject/domain/StatisticsData/WorkerData.java
+++ src/main/java/com/seproject/domain/StatisticsData/WorkerData.java
@@ -9,23 +9,8 @@ public class WorkerData {
     public ArrayList<Double> credit;// 单个任务获得的积分
     public ArrayList<String> missionName; //每个任务的名称
     public int creditRank;
-<<<<<<< HEAD
-    public int workersum;//总工人数
-    public double creditSum; //积分总数
-/*
-    public WorkerData(int missionSum, int finishedMissionNum, int unfinishedMissionNum, ArrayList<Double> credit, int creditRank, int workersum, double creditSum){
-        this.missionSum = missionSum;
-        this.finishedMissionNum = finishedMissionNum;
-        this.unfinishedMissionNum = unfinishedMissionNum;
-        this.credit = credit;
-        this.creditRank = creditRank;
-        this.workersum = workersum;
-        this.creditSum = creditSum;
-    }
-    */
-}
-=======
+
     public int workerSum;//总工人数
     public double  creditSum; //积分总数
 }
->>>>>>> b5a200404d73c7f163a27d769c5305dc38a5bb98
+
