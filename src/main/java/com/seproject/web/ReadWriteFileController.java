953860ac1c4887bf8961e7970b261d963dc83ee7
yrz <161250185@smail.nju.edu.cn>
2018-04-26 10:20:08 +0800

    debug

--- src/main/java/com/seproject/web/ReadWriteFileController.java
+++ src/main/java/com/seproject/web/ReadWriteFileController.java
@@ -65,8 +65,7 @@ public class ReadWriteFileController {
         String picName=jsonObject.getString("picName");
         System.out.println("picName: " + picName);
         String phoneNumber=jsonObject.getString("phoneNumber");
-        Collection collection=collectionService.findByKey(collectionName+phoneNumber);
-        String jsonString=collection.getInfoList().get(Integer.parseInt(picName)-1);
+        Collection collection=collectionService.findByKey(collectionName+phoneNumber);String jsonString=collection.getInfoList().get(Integer.parseInt(picName)-1);
         System.out.println("从后端往前端发送的json字符串：" + jsonString);
         return jsonString;
     }
