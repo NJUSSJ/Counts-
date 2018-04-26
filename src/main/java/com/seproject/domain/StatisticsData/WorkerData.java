6f1c90287ef4acc675dd5adfa31cd79dafc50b79
julia98 <925583000@qq.com>
2018-04-26 21:28:42 +0800

    emmm fix

--- src/main/java/com/seproject/domain/StatisticsData/WorkerData.java
+++ src/main/java/com/seproject/domain/StatisticsData/WorkerData.java
@@ -7,10 +7,10 @@ public class WorkerData {
     public int workerFinishedMissionNum;
     public int workerUnfinishedMissionNum;
     public ArrayList<Double> workerCredit;// 单个任务获得的积分
-    public ArrayList<String> workerMissionName; //每个任务的名称
+    public ArrayList<String> workerMissionName; //每个任务的名称 需要排序(定坐标）
     public ArrayList<Double> workerAverageCredit;//每个任务平均分
     public ArrayList<Double> workerMaxCredit;//每个任务最大分
-    public ArrayList<Integer> workerMissionCreditQuality;//每一项为 [mission,credit,quality]的数组 需排序
+    public ArrayList<ArrayList<Integer>> workerMissionCreditQuality;//每一项为 [mission,credit,quality]的数组 需排序
     public int workerCreditRank;
     public int workerSum;//总工人数
     public double workerCreditSum; //积分总数
