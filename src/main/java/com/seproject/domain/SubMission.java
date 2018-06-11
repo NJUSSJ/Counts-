ce25b969a0b33503f61ec9189f5f69a3bd0ca14f
yrz <161250185@smail.nju.edu.cn>
2018-06-11 21:14:05 +0800

    fix

--- src/main/java/com/seproject/domain/SubMission.java
+++ src/main/java/com/seproject/domain/SubMission.java
@@ -9,58 +9,64 @@ public class SubMission {
       ArrayList<String> uid=new ArrayList<String>();
       int seed=0;//种子
       ArrayList<ArrayList<Integer>> answers=new ArrayList<ArrayList<Integer>>();
-      ArrayList<Integer> goldIndex=new ArrayList<Integer>();//金标索引
+      int id1;
+      int id2;
+
+      public SubMission() {
+      }
 
 
-
-    public SubMission() {
-    }
-
-    public String getKeyID() {
+      public String getKeyID() {
         return keyID;
     }
-
-    public void setKeyID(String keyID) {
+      public void setKeyID(String keyID) {
         this.keyID = keyID;
     }
 
-    public String getMid() {
+      public String getMid() {
         return mid;
     }
 
-    public void setMid(String mid) {
+      public void setMid(String mid) {
         this.mid = mid;
     }
 
-    public ArrayList<String> getUid() {
+      public ArrayList<String> getUid() {
         return uid;
     }
 
-    public void setUid(ArrayList<String> uid) {
+      public void setUid(ArrayList<String> uid) {
         this.uid = uid;
     }
 
-    public int getSeed() {
+      public int getSeed() {
         return seed;
     }
 
-    public void setSeed(int seed) {
+      public void setSeed(int seed) {
         this.seed = seed;
     }
 
-    public ArrayList<ArrayList<Integer>> getAnswers() {
+      public ArrayList<ArrayList<Integer>> getAnswers() {
         return answers;
     }
 
-    public void setAnswers(ArrayList<ArrayList<Integer>> answers) {
+      public void setAnswers(ArrayList<ArrayList<Integer>> answers) {
         this.answers = answers;
     }
 
-    public ArrayList<Integer> getGoldIndex() {
-        return goldIndex;
-    }
+      public int getId1() {
+          return id1;
+      }
 
-    public void setGoldIndex(ArrayList<Integer> goldIndex) {
-        this.goldIndex = goldIndex;
-    }
+      public void setId1(int id1) {
+          this.id1 = id1;
+      }
+      public int getId2() {
+          return id2;
+      }
+
+      public void setId2(int id2) {
+          this.id2 = id2;
+      }
 }
