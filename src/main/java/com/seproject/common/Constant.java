ac0eb272f02b27a0e25d4c61bfceba5400b0506b
yrz <161250185@smail.nju.edu.cn>
2018-06-11 14:33:47 +0800

    计算发起者需要支付的积分

---
+++ src/main/java/com/seproject/common/Constant.java
@@ -0,0 +1,9 @@
+package com.seproject.common;
+
+/**
+ * 常量定义在此
+ */
+public class Constant {
+    public static final int TASK_NUMBER=12;
+    public static final double DISCOUNT_ON_LEVEL=0.05;
+}
---
+++ src/main/java/com/seproject/web/parameter/CallRewardParameter.java
@@ -0,0 +1,34 @@
+package com.seproject.web.parameter;
+
+/**
+ * 计算最高奖励值使用的参数
+ */
+public class CallRewardParameter {
+    private int difficulty;
+    private int maxNum;
+    private String uid;
+
+    public int getMaxNum() {
+        return maxNum;
+    }
+
+    public void setMaxNum(int maxNum) {
+        this.maxNum = maxNum;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+}
