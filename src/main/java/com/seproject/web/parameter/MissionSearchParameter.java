1d2ce726e3b188e480bf8064d946d689b997874b
yrz <161250185@smail.nju.edu.cn>
2018-06-13 23:17:14 +0800

    任务大厅搜索任务

---
+++ src/main/java/com/seproject/web/parameter/MissionSearchParameter.java
@@ -0,0 +1,25 @@
+package com.seproject.web.parameter;
+
+/**
+ * 大厅搜索任务用参数
+ */
+public class MissionSearchParameter {
+    private String range;
+    private String keyword;
+
+    public String getRange() {
+        return range;
+    }
+
+    public void setRange(String range) {
+        this.range = range;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+}
