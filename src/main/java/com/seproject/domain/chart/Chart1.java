51a14fc467feae49b8da14fde1ef0a1961eb4b79
wangruihua <161250143@smail.nju.edu.cn>
2018-06-18 01:30:21 +0800

    V3.7.8
    Signed-off-by: tonywang1945yes <wangruihua88@yahoo.com>

--- src/main/java/com/seproject/domain/chart/Chart1.java
+++ src/main/java/com/seproject/domain/chart/Chart1.java
@@ -6,5 +6,30 @@ public class Chart1 {
     //单个任务每个工人获得的钱数
     String mid="";
     ArrayList<String> uid=new ArrayList<String>();
+
+    public String getMid() {
+        return mid;
+    }
+
+    public void setMid(String mid) {
+        this.mid = mid;
+    }
+
+    public ArrayList<String> getUid() {
+        return uid;
+    }
+
+    public void setUid(ArrayList<String> uid) {
+        this.uid = uid;
+    }
+
+    public ArrayList<Double> getMoney() {
+        return money;
+    }
+
+    public void setMoney(ArrayList<Double> money) {
+        this.money = money;
+    }
+
     ArrayList<Double> money=new ArrayList<Double>();
 }
