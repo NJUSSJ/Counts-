2f84d4391757299ecbbb6cb622de3c463db257e2
tonywang1945yes <wangruihua88@yahoo.com>
2018-06-18 01:10:11 +0800

    1

---
+++ src/main/java/com/seproject/domain/chart/Chart3.java
@@ -0,0 +1,13 @@
+package com.seproject.domain.chart;
+
+import com.seproject.domain.Mission;
+
+import java.util.ArrayList;
+
+public class Chart3 {
+    //每个任务的总收入与总支出统计
+    ArrayList<Mission> missions=new ArrayList<Mission>();
+    ArrayList<Double> pay=new ArrayList<Double>();
+    ArrayList<Double> earn=new ArrayList<Double>();
+
+}
