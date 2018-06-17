885ae0f2b175dd64fe4c03bc99680585870ee76c
julia98 <925583000@qq.com>
2018-06-17 16:59:24 +0800

    fix

--- target/classes/resources/js/LabelEdit.js
+++ target/classes/resources/js/LabelEdit.js
@@ -32,6 +32,31 @@ function nextImage(picNum) {
     var phoneNumber = tmp[2].substring(tmp[2].indexOf("=")+1);
     //var picNum = tmp[3].substring(tmp[3].indexOf("=")+1);
 
+    var answer = loadLabelInfo()[0];
+    if(answer == null){
+        answer = -1;
+    }
+
+    $.ajax({
+        async: false,
+        type: "POST",
+        url: "/uploadLabelMission",
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify({"mid":getNameAndCollection()[0], "uid":getNameAndCollection()[2], "num":picNum, "answer":answer}),
+        success: function (ret) {
+            //alert("state=" + ret);
+            if(ret === 1) {
+                isSubmitted = true;
+                alert("已提交过，请勿重复提交！");
+            }
+        }
+        ,
+        error: function(){
+            //alert("提交失败!");
+        }
+    });
+
     if(Number(i)<picNum) {
         i = Number(i) + 1;
         window.location.href = "test1.html?collection=" + collection + "&imageURL=" + i + "&phoneNumber=" + phoneNumber + "&picNum=" + picNum;
@@ -39,13 +64,13 @@ function nextImage(picNum) {
         alert("本图片为最后一张");
     }
 };
-
+/*
 function LabelData(mid, uid, missionLabel) {
     tmp = window.location.search.split("&");
     this.mid = window.location.search.split("&")[0].substring(tmp[0].indexOf("=")+1);
     this.uid = window.location.search.split("&")[2].substring(tmp[2].indexOf("=")+1);
     this.missionLabel = loadLabelInfo();
-}
+}*/
 
 function updateData() {
     var isSubmitted = false;
@@ -75,10 +100,10 @@ function updateData() {
         $.ajax({
             async: false,
             type: "POST",
-            url: "/updateLabelMission",
+            url: "/commit",
             contentType: "application/json",
             dataType: "json",
-            data: JSON.stringify(new LabelData(getNameAndCollection()[1], getNameAndCollection()[2],loadLabelInfo())),
+            data: JSON.stringify({"keyword":getNameAndCollection()[1], "uid":getNameAndCollection()[2]}),
             success: function (ret) {
                 if(ret === 1)
                     alert("提交成功!");
